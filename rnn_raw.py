e4c62c038ccc2f0481d2a08d294c4d75e83350aa
dare0021 <dare0021@kaist.ac.kr>
2016-07-13 21:40:41 +0900

    Uncommented and edited Keras calls

--- rnn_raw.py
+++ rnn_raw.py
@@ -1,7 +1,7 @@
-# from keras.models import Sequential
-# from keras.layers import Dense, Dropout, Activation, Flatten
-# from keras.layers import Convolution2D, MaxPooling2D
-# from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution1D, MaxPooling1D
+from keras.utils import np_utils
 
 import numpy as np
 import preprocessor as inp
@@ -24,65 +24,49 @@ nb_epoch = 12
 
 ((X_train, y_train), (X_test, y_test)) = inp.run(frameSize, percentageThreshold)
 
-# print X_train.shape, y_train.shape, X_test.shape, y_test.shape
-
-# TODO: find out why the MNIST data is (10000, 1, 28, 28) instead of (10000, 28*28)
-# 		to show there is only one data in the sequence? (there is no sequence, just the image)
-# 		also, shuffle the training data (but not the test)
+print X_train.shape, y_train.shape, X_test.shape, y_test.shape
+print X_train.dtype, y_train.dtype, X_test.dtype, y_test.dtype
 
 # X_train:	input for the training set
 # X_test:	input for the test set
 # y_train:	result for the training set
 # y_test:	result for the test set
 
-# variant number of images, single number per pixel, y size, x size
-# X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
-# X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
-# X_train = X_train.astype('float32')
-# X_test = X_test.astype('float32')
-# # pixel intensity stored as bytes. /=255 to move the range from [0, 255] to [0, 1]
-# X_train /= 255
-# X_test /= 255
-# print('X_train shape:', X_train.shape)
-# print(X_train.shape[0], 'train samples')
-# print(X_test.shape[0], 'test samples')
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
 
-# # convert class vectors to binary class matrices
-# Y_train = np_utils.to_categorical(y_train, nb_classes)
-# Y_test = np_utils.to_categorical(y_test, nb_classes)
+# convert class vectors to binary class matrices
+Y_train = np_utils.to_categorical(y_train, nb_classes)
+Y_test = np_utils.to_categorical(y_test, nb_classes)
 
-# model = Sequential()
+model = Sequential()
 
-# model.add(Convolution2D(nb_filters, nb_conv*8, nb_conv*8,
-#                         border_mode='valid',
-#                         input_shape=(1, img_rows, img_cols)))
-# model.add(Activation('relu'))
-# model.add(Convolution2D(nb_filters, nb_conv*4, nb_conv*4))
-# model.add(Activation('relu'))
-# model.add(Convolution2D(nb_filters, nb_conv*2, nb_conv*2))
-# model.add(Activation('relu'))
-# model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
-# model.add(Dropout(0.25))
+model.add(Convolution1D(nb_filters, nb_conv*8, 
+                        border_mode='valid',
+                        input_shape=(1, X_train.shape[2])))
+model.add(Activation('relu'))
+model.add(MaxPooling1D(pool_size=(nb_pool)))
+model.add(Dropout(0.25))
 
 
-# model.add(Flatten())
-# model.add(Dense(128))
-# model.add(Activation('relu'))
-# model.add(Dropout(0.5))
-# model.add(Dense(nb_classes))
-# model.add(Activation('softmax'))
+model.add(Flatten())
+model.add(Dense(128))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(nb_classes))
+model.add(Activation('softmax'))
 
-# model.compile(loss='categorical_crossentropy',
-#               optimizer='adadelta',
-#               metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy',
+              optimizer='adadelta',
+              metrics=['accuracy'])
 
-# start = time.clock()
-# model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
-#           verbose=0, validation_data=(X_test, Y_test))
-# score = model.evaluate(X_test, Y_test, verbose=0)
-# print(time.clock() - start)
-# print('Test score:', score[0])
-# print('Test accuracy:', score[1])
+start = time.clock()
+model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
+          verbose=0, validation_data=(X_test, Y_test))
+score = model.evaluate(X_test, Y_test, verbose=0)
+print(time.clock() - start)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
 
 # 1) import wav file. DONE
 # 2) carve up wav file in to (overlapping?) fragements by a certain timestep.
@@ -100,3 +84,4 @@ nb_epoch = 12
 # Other: MFC continues to dominate despite being from the 80's, and various modern attempts to create a better feature vector
 # 			PLP is one of the more successful feature filters used along side MFC, despite being from 1990. Ceptra and spectral are more recent (both 2001)
 # 			Different filters can be used to create different feature vectors of the same sample, tripling the samples available and increasing accuracy
+# """
