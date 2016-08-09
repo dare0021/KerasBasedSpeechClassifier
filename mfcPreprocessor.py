Removed automatic rnn_mfcc.evaluator threshold calculation "Fixed" issues with the evaluator not working if the otherGroup was in the test group Since it shouldn't be in the test group anyway, just hardcoded it to only appear in the training group Implemented otherGroup code in speakerInfo

mfcPreprocessor.py
@@ -7,6 +7,10 @@ from keras.utils import np_utils
 
 # speakerID -> 3D array of feature vectors grouped by file
 fileDict = dict()
+# Things that are not from speakers
+# -1: Silences
+otherData = dict()
+otherDataKeys = [-1]
 truthVals = dict()
 
 shuffle = False
@@ -41,6 +45,7 @@ def loadTestSetAuto(rootPath, featureVectorSize = 13):
 # explicitTestSet[0] contains paths, while explicitTestSet[1] contains truth values
 def run(rootPath, percentageThreshold = 0.7, featureVectorSize = 13, explicitTestSet = None):
 	global fileDict
+	global otherData
 	global truthVals
 	global explicit_X_test
 	global explicit_Y_test
@@ -66,6 +71,11 @@ def run(rootPath, percentageThreshold = 0.7, featureVectorSize = 13, explicitTes
 			data = unmfc.run(path, featureVectorSize = featureVectorSize)
 			if sid in fileDict:
 				fileDict[sid].append(data)
+			elif sid in otherData:
+				otherData[sid].append(data)
+			elif sid in otherDataKeys:
+				otherData[sid] = [data.tolist()]
+				truthVals[sid] = sinfo.getTruthValue(path)
 			else:
 				fileDict[sid] = [data.tolist()]
 				truthVals[sid] = sinfo.getTruthValue(path)
@@ -77,12 +87,20 @@ def run(rootPath, percentageThreshold = 0.7, featureVectorSize = 13, explicitTes
 # y: ANN output validation data
 def collateData(speakerList):
 	global fileDict
+	global otherData
 	global truthVals
 
 	x = []
 	y = []
 	for s in speakerList:
-		data = fileDict[s]
+		if s in fileDict:
+			data = fileDict[s]
+		elif s in otherData:
+			data = otherData[s]
+		else:
+			import sys
+			print "ERR: unknown speaker"
+			sys.exit()
 		for f in data:
 			x.extend(f)
 			y.extend(np.full((len(f)), truthVals[s], dtype='int8'))
@@ -103,6 +121,7 @@ def shuffleTwoArrs(x, y):
 # since using Keras dropout would mean feeding everything in to the ANN
 def getSubset(nb_classes, dropout, ratioOfTestsInInput):
 	global fileDict
+	global otherData
 	global truthVals
 	global shuffle
 	global explicit_X_test
@@ -123,6 +142,7 @@ def getSubset(nb_classes, dropout, ratioOfTestsInInput):
 		# TODO: use a 2D set of mfcc data instead of single time steps
 		# Y set should be [None, 1] and match the X set in cardinality
 
+	# Auto
 	if len(explicit_Y_test) <= 0:
 		speakerList = [s for s in fileDict if np.random.uniform(0,1) > dropout]
 		np.random.shuffle(speakerList)
@@ -131,6 +151,9 @@ def getSubset(nb_classes, dropout, ratioOfTestsInInput):
 		speakersTrain, speakersTest = np.split(speakerList, [trainListSize])
 		testSpeakers = speakersTest
 
+		otherGroup = []
+		otherGroup.extend(otherData)
+		speakersTrain = np.append(speakersTrain, otherGroup)
 		X_train, Y_train = collateData(speakersTrain)
 		X_test, Y_test = collateData(speakersTest)
 
@@ -143,18 +166,14 @@ def getSubset(nb_classes, dropout, ratioOfTestsInInput):
 
 		X_train = np.array(X_train, dtype='float32')
 		X_test = np.array(X_test, dtype='float32')
-
-		X_train = X_train.reshape(X_train.shape[0], 1, X_train.shape[1], 1)
-		X_test = X_test.reshape(X_test.shape[0], 1, X_test.shape[1], 1)
-
-		print "Training set:", speakersTrain
-		print "Testing  set:", speakersTest
-
-		return X_train, Y_train, X_test, Y_test
+	# Manual explicit test group
 	else:
 		speakerList = [s for s in fileDict if np.random.uniform(0,1) > dropout]
 		np.random.shuffle(speakerList)
 
+		otherGroup = []
+		otherGroup.extend(otherData)
+		speakersTrain = np.append(speakersTrain, otherGroup)
 		X_train, Y_train = collateData(speakerList)
 
 		if shuffle is True:
@@ -166,13 +185,13 @@ def getSubset(nb_classes, dropout, ratioOfTestsInInput):
 		X_train = np.array(X_train, dtype='float32')
 		X_test = np.array(explicit_X_test, dtype='float32')
 
-		X_train = X_train.reshape(X_train.shape[0], 1, X_train.shape[1], 1)
-		X_test = X_test.reshape(X_test.shape[0], 1, X_test.shape[1], 1)
+	X_train = X_train.reshape(X_train.shape[0], 1, X_train.shape[1], 1)
+	X_test = X_test.reshape(X_test.shape[0], 1, X_test.shape[1], 1)
 
-		print "Training set:", speakersTrain
-		print "Testing  set:", speakersTest
-		
-		return X_train, Y_train, X_test, Y_test
+	print "Training set:", speakersTrain
+	print "Testing  set:", speakersTest
+	
+	return X_train, Y_train, X_test, Y_test
 
 
 # for unit test