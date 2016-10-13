be79da35df9d4ebef48eb6798769ace70fef4525
dare0021 <dare0021@kaist.ac.kr>
2016-10-13 19:19:31 +0900

    Graphing done Now to see if I can find a graph that people can read

--- outputVisualizer.py
+++ outputVisualizer.py
@@ -1,9 +1,14 @@
-import numpy as numpy
+import numpy as np
 import matplotlib.pyplot as plt
 
-input = "/home/jkih/projects/KerasBasedSpeechClassifier/saveData_Dropout/10mA_0.761203902762.txt"
+cj60ba = "/home/jkih/projects/KerasBasedSpeechClassifier/saveData_Dropout/CJ60A_dither.txt"
+cj60bc = "/home/jkih/projects/KerasBasedSpeechClassifier/saveData_Dropout/CJ60C_dither.txt"
+
+input = cj60ba
+target = 0
 
 data = []
+dataMax = []
 
 def readData(intput):
 	global data
@@ -21,5 +26,60 @@ def readData(intput):
 
 		s = f.readline()
 
+def getMax(nb_classes):
+	global dataMax
+	dataMax = []
+	for arr in data:
+		dataMax.append(np.argmax(arr[:nb_classes]))
+
+def getAccuracy(silentTreatment, target):
+	global dataMax
+	sum = 0.0
+	total = len(data)
+	if silentTreatment == "ignore silence":
+		getMax(2)
+		for i in dataMax:
+			if i == target:
+				sum += 1
+	elif silentTreatment == "silence drop":
+		getMax(3)
+		total = 0
+		for i in dataMax:
+			if i == target:
+				sum += 1
+				total += 1
+			elif i != 2:
+				total += 1
+	elif silentTreatment == "strictly no silence":
+		getMax(3)
+		for i in dataMax:
+			if i == target:
+				sum += 1
+	else:
+		sum = -1
+		print "Unknown silentTreatment in outputVisualizer.getAccuracy()"
+	print sum, '/', total, '=',sum / total, '\t',silentTreatment
+	return sum / total
+
+# savePath can be png or pdf
+def getRawGraph(nb_classes, savePath = "", verbose = True):
+	global data
+	xaxis = np.arange(0, len(data))
+	plt.figure(1)
+	plt.subplot(111)	
+	npcopy = np.array(data)
+	for i in range(0, nb_classes):
+		plt.plot(xaxis, npcopy[:,i], label='C'+str(i))
+	plt.legend(loc='center right')
+	if not verbose:
+		plt.ioff()
+	if savePath != "":
+		plt.savefit(savePath, bbox_inches='tight')
+	else:
+		plt.show()
+
 readData(input)
-print data
+getAccuracy("ignore silence", target)
+getAccuracy("silence drop", target)
+getAccuracy("strictly no silence", target)
+getRawGraph(3)
